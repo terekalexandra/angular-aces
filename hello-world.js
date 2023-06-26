a3ec2d63be419d8a6c5e91e29981e8c841740618
terekalexandra <terekalexandra@gmail.com>
2023-06-26 14:34:42 +0200

    adding a5 again

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,6 @@
-function greet(greeting,name) {
-   console.log(greeting+" "+name+"!");
-}
+{
+    function greet(greeting,name)
+       console.log(greeting+" "+name+"!");}
 
 let greeting=Howdy;
 let name=Alex;
